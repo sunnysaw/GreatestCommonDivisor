752b0224ab5f69c8892991879394d4ed5f014153
sunny <sunnyroy8084401051@gmail.com>
2023-03-28 20:20:57 +0530

    commit :

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,24 @@ Enter First Number:24
 Enter Second Number:16
 Greatest Common Divisor:8
 */
+import static java.lang.StrictMath.min;
+
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int FirstDigit,SecondDigit,Multiple = 1,temp = 2;
+        int FirstDigit,SecondDigit,Multiple = 1,i;
         System.out.println("Enter the FirstDigit :");
         FirstDigit = sc.nextInt();
         System.out.println("Enter the SecondDigit :");
         SecondDigit = sc.nextInt();
-        while (FirstDigit % 2 == 0 && SecondDigit % 2 == 0){
-            FirstDigit = FirstDigit / 2;
-            SecondDigit = SecondDigit / 2;
-            Multiple = Multiple * temp;
+        int n = min(FirstDigit,SecondDigit);
+        for (i = 2; i < n; i++){
+            while (FirstDigit % i == 0 && SecondDigit % i == 0){
+                FirstDigit = FirstDigit / 2;
+                SecondDigit = SecondDigit / 2;
+                Multiple = Multiple * i;
+            }
         }
         System.out.println("Printing the result : " + Multiple);
     }
